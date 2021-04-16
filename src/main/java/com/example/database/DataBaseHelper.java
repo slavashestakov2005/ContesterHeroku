11a571444b2eaff195c1f06e8a2f5e62684261c3
slavashestakov2005 <slavashestakov2005@gmail.com>
2021-04-16 18:34:28 +0700

    Добавление БД.

--- src/main/java/com/example/database/DataBaseHelper.java
+++ src/main/java/com/example/database/DataBaseHelper.java
@@ -10,7 +10,8 @@ public class DataBaseHelper {
     static {
         try {
             Class.forName("org.postgresql.Driver");
-            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+            String dbUrl = System.getenv("JDBC_DATABASE_URL") + "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+            System.out.println(dbUrl);
             connection = DriverManager.getConnection(dbUrl);
 
             statements = new Stack<>();
